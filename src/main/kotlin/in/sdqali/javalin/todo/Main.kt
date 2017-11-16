b673ac6fe576c16606efe771e665be79c8f18950
Sadique Ali <sadiqalikm@gmail.com>
2017-11-15 21:24:01 -0800

    Added Javalin.

---
+++ src/main/kotlin/in/sdqali/javalin/todo/Main.kt
@@ -0,0 +1,10 @@
+package `in`.sdqali.javalin.todo
+
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.start(7000)
+    app.get("/") {
+        ctx -> ctx.result("Hello World")
+    }
+}
