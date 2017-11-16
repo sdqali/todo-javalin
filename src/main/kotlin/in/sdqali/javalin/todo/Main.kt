09de551bd8def6848fba9e0257d871bf64d08a6d
Sadique Ali <sadiqalikm@gmail.com>
2017-11-15 21:24:14 -0800

    Enable CORS for everyone.

--- src/main/kotlin/in/sdqali/javalin/todo/Main.kt
+++ src/main/kotlin/in/sdqali/javalin/todo/Main.kt
@@ -1,10 +1,20 @@
 package `in`.sdqali.javalin.todo
 
 import io.javalin.Javalin
+import io.javalin.core.util.Header
 
 fun main(args: Array<String>) {
-    val app = Javalin.start(7000)
+    val app = Javalin.create()
+        .enableCorsForOrigin()
+        .start()
+    app.before("*") { ctx ->
+        (ctx.header(Header.ORIGIN) ?: ctx.header(Header.REFERER))?.let {
+            ctx.header(Header.ACCESS_CONTROL_ALLOW_ORIGIN, it)
+        }
+    }
+
     app.get("/") {
         ctx -> ctx.result("Hello World")
     }
 }
+
